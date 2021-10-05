427efb6265fcff3ea468fae0e0d8df046366c480
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2021-10-05 17:36:58 +0200

    update rock paper scissors

--- sessie1/src/rockpaperscissors.kt
+++ sessie1/src/rockpaperscissors.kt
@@ -1,3 +1,19 @@
 fun main() {
+    val options = arrayOf("Rock", "Paper", "Scissors");
+    val computerChoice = options.random();
+    val userChoice = getUserChoice(options)
 
+    println("Computer picked $computerChoice");
+    println("User picked $userChoice");
 }
+
+fun getUserChoice(options: Array<String>): String? {
+    var userChoice: String?
+    do {
+        println("Make your choice: Rock - Paper - Scissors");
+        userChoice = readLine();
+    } while (!options.contains(userChoice))
+    return userChoice
+}
+
+fun DetermineWinner(computerChoice: )
