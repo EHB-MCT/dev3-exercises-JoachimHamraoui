2d335581c1d35afb274c7695b387321520c9b3bd
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2021-10-06 14:57:13 +0200

    finished exercise

--- sessie1/src/rockpaperscissors.kt
+++ sessie1/src/rockpaperscissors.kt
@@ -5,6 +5,7 @@ fun main() {
 
     println("Computer picked $computerChoice");
     println("User picked $userChoice");
+    determineWinner(computerChoice, userChoice);
 }
 
 fun getUserChoice(options: Array<String>): String? {
@@ -16,4 +17,21 @@ fun getUserChoice(options: Array<String>): String? {
     return userChoice
 }
 
-fun DetermineWinner(computerChoice: )
+fun determineWinner(computerChoice: String, userChoice: String?) {
+
+    if (computerChoice == userChoice) {
+        println("It's a Draw")
+        return
+    }
+
+    val winMessage = "You've won"
+    val loseMessage = "You've lost"
+
+    when (computerChoice) {
+        "Rock" -> if (userChoice == "Paper") println(winMessage) else println(loseMessage)
+        "Paper" -> if (userChoice == "Scissors") println(winMessage) else println(loseMessage)
+        "Scissors" -> if (userChoice == "Rock") println(winMessage) else println(loseMessage)
+    }
+}
+
+
