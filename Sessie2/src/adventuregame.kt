5bd1b856fd795d7f02c92d30d237079ffac361d3
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2021-10-12 16:30:26 +0200

    commit

--- Sessie2/src/adventuregame.kt
+++ Sessie2/src/adventuregame.kt
@@ -1,13 +1,13 @@
 fun main() {
     println("Welcome to The World That Never Was, your journey with the Keyblade has led you to the domain of Organization XIII");
-    println("Do you wish to stop here or to continue?");
     getUserChoice();
+    Test();
 }
 
 fun getUserChoice() {
     val userChoice: String?
     userChoice = readLine();
-
+    println("Do you wish to stop here or to continue?");
     if (userChoice == "Continue") {
         println("You have chosen to go further on your quest. There is no point of return.")
     } else if (userChoice == "Stop here") {
