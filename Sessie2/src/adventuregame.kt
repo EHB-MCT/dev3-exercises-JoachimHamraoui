36e8e5825b4cce144035bf7621c4bca45f009c5f
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2021-10-12 21:10:05 +0200

    .

--- Sessie2/src/adventuregame.kt
+++ Sessie2/src/adventuregame.kt
@@ -1,10 +1,9 @@
 fun main() {
     println("Welcome to The World That Never Was, your journey with the Keyblade has led you to the domain of Organization XIII");
-    getUserChoice();
-    Test();
+    Challenge1();
 }
 
-fun getUserChoice() {
+fun Challenge1() {
     val userChoice: String?
     userChoice = readLine();
     println("Do you wish to stop here or to continue?");
@@ -12,5 +11,17 @@ fun getUserChoice() {
         println("You have chosen to go further on your quest. There is no point of return.")
     } else if (userChoice == "Stop here") {
         println("You're probably going to do some sidequests")
+    }
+    Challenge2();
+}
+
+fun Challenge2() {
+    val userChoice: String?
+    userChoice = readLine();
+    println("You've reached the main dungeon, but there are two entry points. Which entrance will you choose?");
+    if (userChoice == "Left") {
+        println("You died homie")
+    } else if (userChoice == "Right") {
+        println("You chose the RIGHT way ahaha you entered the dungeon")
     }
 }
